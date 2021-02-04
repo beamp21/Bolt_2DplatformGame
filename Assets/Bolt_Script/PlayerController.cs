567a81dc8081146afefacceac87f26c75ff9d495
Florian <florian.duruz@gmail.com>
2021-02-05 00:03:04 +0100

    Avancement Death/spike

--- Assets/Bolt_Script/PlayerController.cs
+++ Assets/Bolt_Script/PlayerController.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 5f;
+    public float Jump = 12f;
     private float _baseSpeed;
     private float _mouvementSpeed;
     private GameObject _player;
     private Rigidbody2D _playerRigidBody;
     private Vector3 _flipPlayer;
     private Animator _playerAnimator;
+    private RaycastHit2D _playerRaycast;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,16 +37,23 @@ public class PlayerController : MonoBehaviour
         {
             _baseSpeed = Input.GetAxis("Horizontal");
         }
+        _playerRaycast = Physics2D.CircleCast(_player.transform.position, 0.3f, new Vector2(0, -1), 1.1f, LayerMask.GetMask("Platforms"));
+        bool _playerGrounded = _playerRaycast.collider != null ? true : false;
+        if(Input.GetButtonDown("Jump") && _playerGrounded)
+        {
+            //Add instant force to Y for the jump
+            _playerRigidBody.AddForce(new Vector2(0,Jump), ForceMode2D.Impulse);
+        }
         _mouvementSpeed = _baseSpeed * Speed;
         _playerRigidBody.velocity = new Vector2(_mouvementSpeed, _player.GetComponent<Rigidbody2D>().velocity.y);
 
         // condition ? valeur si vrai : valeur si faux;
         _flipPlayer.x = _mouvementSpeed < 0 ? -1 : 1;
-
         if (_mouvementSpeed != 0)
         {
             _player.transform.localScale = _flipPlayer;
         }
         _playerAnimator.SetFloat("Speed", Mathf.Abs(_mouvementSpeed));
+        _playerAnimator.SetBool("Grounded", _playerGrounded);
     }
 }
